edb4c4878806e4736bd39f3563d5062d14b9f600
Milen <milenmihalkov@gmail.com>
2023-01-11 15:47:28 +0200

    Add data-price attr

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let price = document.getElementsByClassName("price");
+  let parent = document.getElementsByClassName("product");
 
+  for(let i=0; i<parent.length; i++){
+    parent[i].setAttribute('data-price', price[0].innerHTML);
+  }
 
 });
